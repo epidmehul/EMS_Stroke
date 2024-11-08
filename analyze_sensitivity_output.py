2fbcf80a4b74d21113d20147226eac73b170bbd4
plin97 <peterlin1997@gmail.com>
2024-11-08 11:31:31 -0500

    Separated out sensitivity analyses into different CSV files

--- analyze_sensitivity_output.py
+++ analyze_sensitivity_output.py
@@ -13,20 +13,18 @@ for path in map_files:
     df = read_output(path, save_format = 'parquet')
 
     map_num = int(path.stem.split('_')[-1])
+    sens_descriptor = '_'.join(path.stem.split('_')[:-1]).rstrip('_map')
 
-    data_calcs_csv_path = pathlib.Path('/work/users/p/w/pwlin/output_sens/map_scenario_threshold_seed_results.csv')
-    if not data_calcs_csv_path.parent.exists():
-        data_calcs_csv_path.parent.mkdir(parents = True)
-    elif data_calcs_csv_path.exists():
-        data_calcs_csv_path.unlink()
+    data_calcs_csv_path = pathlib.Path('/work/users/p/w/pwlin/output_sens/all_numbers')
+
+
+    if not data_calcs_csv_path.exists():
+        data_calcs_csv_path.mkdir(parents = True)
 
     maps_csv_path = pathlib.Path('/work/users/p/w/pwlin/output_sens/maps.csv')
     if maps_csv_path.exists():
         maps_csv_path.unlink()
 
-    result = single_map_analysis_output(df, map_number = map_num, heatmap_diff = True, save = True, output_dir_str = '/work/users/p/w/pwlin/output_sens/results', line_errorbars = True, additional_file_name = '_'.join(path.stem.split('_')[:-1])) 
+    result = single_map_analysis_output(df, map_number = map_num, heatmap_diff = True, save = True, output_dir_str = '/work/users/p/w/pwlin/output_sens/results', line_errorbars = True, additional_file_name = sens_descriptor) 
 
-    if data_calcs_csv_path.exists():
-        result.to_csv(data_calcs_csv_path, header = False, index = False, mode = 'a')
-    else:
-        result.to_csv(data_calcs_csv_path, header = True, index = False, mode = 'w')
+    result.to_csv(data_calcs_csv_path / path.stem, header = True, index = False, mode = 'w')
