4cfed5770f9fb5ab0f97c284999c36e50f4bb203
plin97 <peterlin1997@gmail.com>
2024-11-08 12:41:41 -0500

    Fixed maps.csv being overwritten for sensitivity analysis

--- analyze_sensitivity_output.py
+++ analyze_sensitivity_output.py
@@ -9,21 +9,21 @@ import pathlib
 parquet_files = pathlib.Path('/work/users/p/w/pwlin/output_sens/parquet_files')
 map_files = parquet_files.glob('*.parquet')
 
+data_calcs_csv_path = pathlib.Path('/work/users/p/w/pwlin/output_sens/all_numbers')
+
+if not data_calcs_csv_path.exists():
+    data_calcs_csv_path.mkdir(parents = True)
+
+maps_csv_path = pathlib.Path('/work/users/p/w/pwlin/output_sens/maps.csv')
+if maps_csv_path.exists():
+    maps_csv_path.unlink()
+
+
 for path in map_files:
     df = read_output(path, save_format = 'parquet')
 
     map_num = int(path.stem.split('_')[-1])
     sens_descriptor = '_'.join(path.stem.split('_')[:-1]).rstrip('_map')
-
-    data_calcs_csv_path = pathlib.Path('/work/users/p/w/pwlin/output_sens/all_numbers')
-
-
-    if not data_calcs_csv_path.exists():
-        data_calcs_csv_path.mkdir(parents = True)
-
-    maps_csv_path = pathlib.Path('/work/users/p/w/pwlin/output_sens/maps.csv')
-    if maps_csv_path.exists():
-        maps_csv_path.unlink()
 
     result = single_map_analysis_output(df, map_number = map_num, heatmap_diff = True, save = True, output_dir_str = '/work/users/p/w/pwlin/output_sens/results', line_errorbars = True, additional_file_name = sens_descriptor) 
 
