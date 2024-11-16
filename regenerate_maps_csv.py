446d25db6e98fde5708113367812d5369598d3d1
plin97 <peterlin1997@gmail.com>
2024-11-16 14:12:56 -0500

    Fixed pathlib.Path format string

--- regenerate_maps_csv.py
+++ regenerate_maps_csv.py
@@ -10,5 +10,5 @@ maps_nums = [31, 93, 126, 244, 414, 671, 701, 703, 711, 984]
 
 if __name__ == '__main__':
     for map_seed in maps_nums:
-        map_specific_path = args.path / 'map_{str(map_number).zfill(3)}'
+        map_specific_path = args.path / f'map_{str(map_seed).zfill(3)}'
         generate_maps_csv(map_seed, map_specific_path)
