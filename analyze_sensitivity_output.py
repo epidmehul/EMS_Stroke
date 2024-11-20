fc7f3865ab1a36b996569e5e1adb9f8f71def394
plin97 <peterlin1997@gmail.com>
2024-11-20 09:57:37 -0500

    Added try except to sensitivity analysis for possible ignore of map 985

--- analyze_sensitivity_output.py
+++ analyze_sensitivity_output.py
@@ -1,6 +1,9 @@
 from postprocess_simulation_results import *
 import pathlib
 import multiprocessing as mp
+import warnings
+
+warnings.filterwarnings('ignore')
 
 # os.chdir('/proj/patellab/Sheps/output')
 # os.chdir('/work/users/p/w/pwlin')
@@ -20,14 +23,18 @@ if maps_csv_path.exists():
     maps_csv_path.unlink()
 
 def single_map_sensitivitiy_analyses(path):
-    df = read_output(path, save_format = 'parquet')
+    try:
+        df = read_output(path, save_format = 'parquet')
 
-    map_num = int(path.stem.split('_')[-1])
-    sens_descriptor = '_'.join(path.stem.split('_')[:-1]).rstrip('_map')
+        map_num = int(path.stem.split('_')[-1])
+        sens_descriptor = '_'.join(path.stem.split('_')[:-1]).rstrip('_map')
 
-    result = single_map_analysis_output(df, map_number = map_num, heatmap_diff = True, save = True, output_dir_str = '/work/users/p/w/pwlin/full_output_sens/results', line_errorbars = True, additional_file_name = sens_descriptor) 
+        result = single_map_analysis_output(df, map_number = map_num, heatmap_diff = True, save = True, output_dir_str = '/work/users/p/w/pwlin/full_output_sens/results', line_errorbars = True, additional_file_name = sens_descriptor) 
 
-    result.to_csv(data_calcs_csv_path / (path.stem + '.csv'), header = True, index = False, mode = 'w')
+        result.to_csv(data_calcs_csv_path / (path.stem + '.csv'), header = True, index = False, mode = 'w')
+    except:
+        print('failure',path)
+        pass
 
 if __name__ == '__main__':
     with mp.Pool(20) as pool:
