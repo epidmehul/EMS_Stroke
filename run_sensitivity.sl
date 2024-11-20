8cc81681b455dc5b617424712276ccf92048d33c
plin97 <peterlin1997@gmail.com>
2024-11-19 19:53:46 -0500

    Added multiprocessing code for full sensitivity runs

--- run_sensitivity.sl
+++ run_sensitivity.sl
@@ -2,15 +2,15 @@
 
 #SBATCH -p general
 #SBATCH -N 1
-#SBATCH -n 1
-#SBATCH --mem=4g
-#SBATCH -t 0:05:00
+#SBATCH -n 11
+#SBATCH --mem=10g
+#SBATCH -t 02:00:00
 #SBATCH -o /nas/longleaf/home/pwlin/strokes/output.out
-#SBATCH -J lvo_senss
+#SBATCH -J full_sens_run
 #SBATCH --mail-type=end
 #SBATCH --mail-user=pwlin@live.unc.edu
 
 module purge
 module load python/3.12.2
 source ~/strokes_venv/bin/activate
-python run_simulation_sensitivity_multiprocess.py -s 40 -p 1000
+python run_simulation_sensitivity_multiprocess.py -s 40 -p 1000 -o /work/users/p/w/pwlin/full_output_sens/parquet_files
--- run_simulation_sensitivity_multiprocess.py
+++ run_simulation_sensitivity_multiprocess.py
@@ -7,6 +7,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-s', '--seeds', help = 'number of random seeds', type = int, default = 40)
 parser.add_argument('-p', '--patients', help = 'number of patients', type = int, default = 1000)
 parser.add_argument('-c', '--config', help = 'config file with simulation parameters', type = pathlib.Path, default = None)
+parser.add_argument('-o','--output', help = 'output directory for parquet files', type = pathlib.Path, default = None)
 args = parser.parse_args()
 
 # os.chdir('/proj/patellab/Sheps/output')
@@ -17,7 +18,12 @@ args = parser.parse_args()
 # map_seeds = [711, 31, 244, 126, 701, 671, 93, 414, 984]
 # map_seeds = [703]
 map_seeds = [i for i in range(1000)]
-output_dir = '/work/users/p/w/pwlin/output_sens/parquet_files'
+
+if args.output is None:
+    output_dir = pathlib.Path('/work/users/p/w/pwlin/output_sens/parquet_files')
+else:
+    output_dir = args.output
+
 
 def run_single_map(map_seed):
     run_map_simulations([map_seed], num_patients = 1000, num_patient_seeds = 40, save_format = 'parquet', output_dir = output_dir, config = {'patients_lvo_ischemic': 0.141}, additional_file_name='low_lvo')
@@ -30,8 +36,10 @@ if __name__ == '__main__':
     output_dir_path = pathlib.Path(output_dir)
     if not output_dir_path.exists():
         output_dir_path.mkdir(parents = True)
+    with mp.Pool(10) as pool:
+        pool.map(run_single_map, map_seeds)
     
-    
+
     # for map in map_seeds:
     #     run_map_simulations([map], num_patients = 1000, num_patient_seeds = 40, save_format = 'parquet', output_dir = output_dir, config = {'patients_lvo_ischemic': 0.141}, additional_file_name='low_lvo')
 
