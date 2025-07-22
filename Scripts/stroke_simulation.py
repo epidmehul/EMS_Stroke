216e632a16f498cf9a65ab15ecefebd87d46909f
Peter Lin <peterlin1997@gmail.com>
2025-07-22 09:13:17 -0400

    Removed try block to fix simulation issue with 0 width ci

--- Scripts/stroke_simulation.py
+++ Scripts/stroke_simulation.py
@@ -440,16 +440,16 @@ def simulation(num_patients, patient_seed, map_seed, sens_spec_vals = np.array([
         )
         
     ### Randomizing whether or not a patient receives IVT
-    try:
-        IVTtreatment = ischemic_arr & (IVTtime < ivt_time_threshold) & (rng.random(IVTtime.shape) < ivt_probability) & pd.Series(destination_arr.flatten()).str.contains('[' + config['csc_prefix'] + '|' + config['psc_prefix'] + ']').values.reshape((num_patients, num_scenarios, num_thresholds))
+    # try:
+    IVTtreatment = ischemic_arr & (IVTtime < ivt_time_threshold) & (rng.random(IVTtime.shape) < ivt_probability) & pd.Series(destination_arr.flatten()).str.contains('[' + config['csc_prefix'] + '|' + config['psc_prefix'] + ']').values.reshape((num_patients, num_scenarios, num_thresholds))
 
-        IVTrepurfusion = ischemic_arr & IVTtreatment & (rng.random(IVTtreatment.shape) < early_repurfusion_probability)
+    IVTrepurfusion = ischemic_arr & IVTtreatment & (rng.random(IVTtreatment.shape) < early_repurfusion_probability)
 
-        EVTtreatment = lvo_status_arr & (EVTtime < evt_time_threshold) & (rng.random(EVTtime.shape) < evt_probability) & ~IVTrepurfusion
-    except:
-        IVTtreatment = np.ones_like(lvo_status_arr)
-        IVTrepurfusion = np.zeros_like(lvo_status_arr)
-        EVTtreatment = np.ones_like(lvo_status_arr)
+    EVTtreatment = lvo_status_arr & (EVTtime < evt_time_threshold) & (rng.random(EVTtime.shape) < evt_probability) & ~IVTrepurfusion
+    # except:
+    #     IVTtreatment = np.ones_like(lvo_status_arr)
+    #     IVTrepurfusion = np.zeros_like(lvo_status_arr)
+    #     EVTtreatment = np.ones_like(lvo_status_arr)
 
 
     ### Updating risk equations for mRS 0-2
