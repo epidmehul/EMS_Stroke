105ced28270d8ae9c38221e0ab097790dedba4e5
Peter Lin <peterlin1997@gmail.com>
2025-08-27 02:00:49 -0400

    testing output df saving

--- Scripts/run_sampson_simulation.py
+++ Scripts/run_sampson_simulation.py
@@ -45,7 +45,7 @@ def run_analyze(map_seed):
     df = run_map_simulations([map_seed], num_patients = args.patients, num_patient_seeds = args.seeds, save_format = 'parquet', output_dir = output_dir / 'parquet_files', config = config_dict)
 
     print(df.shape)
-    df.to_parquet('~/test.parquet')
+    df.to_parquet(output_dir / 'parquet_files' / 'test.parquet')
     
     cohort_avgs, intervals = process_data(df = df, output_dir = output_dir / 'results', map_number = map_seed, save_format = 'csv', psc_only = False, config = config_dict, errorbars = True)
 
