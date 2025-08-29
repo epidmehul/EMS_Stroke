46ebe081837fb8701ec6473d1b822b8f9636d942
Peter Lin <peterlin1997@gmail.com>
2025-08-29 16:50:06 -0400

    changed evt proportion plot to default matplotlib

--- Scripts/postprocess_simulation_results.py
+++ Scripts/postprocess_simulation_results.py
@@ -876,6 +876,7 @@ def process_data(filepath = None, plots = True, errorbars = False, additional_fi
         prop_plots.savefig(output_dir / f'map_{str(map_number).zfill(3)}' / f'{'psc_' if psc_only else ''}{additional_file_name if additional_file_name is not None else ''}{'_' if additional_file_name != '' else ''}map_{map_number}_treatment_proportions_plot.png')
 
         plt.close()
+        plt.style.use('default')
         evt_time_props.plot(kind = 'bar', stacked = True, figsize = (12, 8))
         plt.savefig(output_dir / f'map_{str(map_number).zfill(3)}'/ f'{'psc_' if psc_only else ''}{additional_file_name if additional_file_name is not None else ''}{'_' if additional_file_name != '' else ''}map_{map_number}_evt_time_props.png')
     return joined_avgs, intervals_df
