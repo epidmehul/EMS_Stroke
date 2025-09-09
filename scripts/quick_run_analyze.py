8bd033069a116c930b9096f6143cc8233b78c3de
Peter Lin <peterlin1997@gmail.com>
2025-09-09 16:14:16 -0400

    cleaned up quick run output

--- scripts/quick_run_analyze.py
+++ scripts/quick_run_analyze.py
@@ -12,7 +12,7 @@ parser.add_argument('-c', '--config', help = 'config file with simulation parame
 parser.add_argument('-d', '--data', help = 'data file containing patient information on hexes and LKW times', type = pathlib.Path, default = None)
 parser.add_argument('-t', '--times', help = 'data file containing travel times from hexes and hospitals to hospitals', type = pathlib.Path, default = None)
 parser.add_argument('-m', '--map_seed', help = 'map number to save results under', type = int, default = 0)
-parser.add_argument('-o', '--output', help = 'output directory for simulation results', type = pathlib.Path, default = pathlib.Path(__file__).parent / 'output')
+parser.add_argument('-o', '--output', help = 'output directory for simulation results', type = pathlib.Path, default = pathlib.Path(__file__).parent.parent / 'output')
 args = parser.parse_args()
 
 if __name__ == '__main__':
@@ -25,6 +25,6 @@ if __name__ == '__main__':
                              save_format = 'parquet',
                              additional_file_name = None,
                              )
-    process_data(df = df, 
-                 output_dir = args.output / 'results',
-                 map_number = args.map_seed)
+    # process_data(df = df, 
+                output_dir = args.output / 'results',
+                map_number = args.map_seed)
